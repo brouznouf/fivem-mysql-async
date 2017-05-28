59630fa115ad726876bbe9d254c819ed7df47212
Joel Wurtz <jwurtz@jolicode.com>
2017-05-28 02:55:41 +0200

    Add connection pool clear

--- lib/MySQL.lua
+++ lib/MySQL.lua
@@ -21,12 +21,25 @@ require "resources/mysql-async/lib/Sync"
 -- @param password
 --
 function MySQL.init(self)
+    local isInit = false
+
+    -- Very Ugly not to be done better
+    foreach assembly in clr.System.AppDomain.CurrentDomain.GetAssemblies() do
+        if assembly.ToString() == "Async, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null" then
+            isInit = true
+        end
+    end
+
     local reflection = clr.System.Reflection
 
     reflection.Assembly.LoadFrom('resources/mysql-async/lib/MySqlConnector.dll')
     reflection.Assembly.LoadFrom('resources/mysql-async/lib/Async.dll')
 
     self.mysql = clr.MySql.Data.MySqlClient
+    self.isInit = true;
+    self.settings = clr.MySql.Data.MySqlClient.MySqlConnectionStringBuilder("server="..self.Config.Host..";database="..self.Config.Database..";userid="..self.Config.User..";password="..self.Config.Password.."")
+
+    return self.mysql, isInit
 end
 
 ---
@@ -35,10 +48,23 @@ end
 -- @param self
 --
 function MySQL.createConnection(self)
-    local connection = self.mysql.MySqlConnection("server="..self.Config.Host..";database="..self.Config.Database..";userid="..self.Config.User..";password="..self.Config.Password.."")
+    local connection = self.mysql.MySqlConnection(self.settings)
     connection.Open()
 
     return connection
 end
 
-MySQL:init()
+local function ClearPoolLoop(mysql)
+    SetTimeout(180000, function()
+        Logger:Info('Clear MySQL connection pool')
+        mysql.MySqlConnection.ClearAllPools()
+
+        ClearPoolLoop(mysql)
+    end)
+end
+
+local mysql, isInit = MySQL:init()
+
+if not isInit then
+    ClearPoolLoop(mysql)
+end
