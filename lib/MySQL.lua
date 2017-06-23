4002465a3d69cb8011b96ab73b20abeaeea3606a
Joel Wurtz <joel.wurtz@gmail.com>
2017-06-23 20:43:51 +0200

    Separate init into his own file to avoid multiples start event on mysql, add assert to prevent errors

--- lib/MySQL.lua
+++ lib/MySQL.lua
@@ -10,6 +10,9 @@ local function safeParameters(params)
         return {[''] = ''}
     end
 
+    assert(type(params) == "table", "A table is expected")
+    assert(params[1] == nil, "Parameters should not be an array, but a map (key / value pair) instead")
+
     if next(params) == nil then
         return {[''] = ''}
     end
@@ -17,34 +20,77 @@ local function safeParameters(params)
     return params
 end
 
+local function safeCallback(callback)
+    if nil == callback then
+        return function() end
+    end
+
+    assert(type(callback) == "function", "A callback is expected")
+
+    return callback
+end
+
+---
+-- Init mysql
+--
 function MySQL.init(self)
     exports['mysql-async']:mysql_configure(self.Config.Host, self.Config.User, self.Config.Password, self.Config.Database)
 end
 
+---
+-- Execute a query with no result required, sync version
+--
+-- @param query
+-- @param params
+--
+-- @return int Number of rows updated
+--
 function MySQL.Sync.execute(query, params)
+    assert(type(query) == "string", "The SQL Query must be a string")
+
     return exports['mysql-async']:mysql_sync_execute(query, safeParameters(params))
 end
 
+---
+-- Execute a query and fetch all results in an sync way
+--
+-- @param query
+-- @param params
+--
+-- @return table Query results
+--
 function MySQL.Sync.fetchAll(query, params)
+    assert(type(query) == "string", "The SQL Query must be a string")
+
     return exports['mysql-async']:mysql_sync_fetch_all(query, safeParameters(params))
 end
 
+---
+-- Execute a query and fetch the first column of the first row, sync version
+-- Useful for count function by example
+--
+-- @param query
+-- @param params
+--
+-- @return mixed Value of the first column in the first row
+--
 function MySQL.Sync.fetchScalar(query, params)
+    assert(type(query) == "string", "The SQL Query must be a string")
+
     return exports['mysql-async']:mysql_sync_fetch_scalar(query, safeParameters(params))
 end
 
 ---
--- Execute a query with no result required
+-- Execute a query with no result required, async version
 --
 -- @param query
 -- @param params
--- @param func
--- @param Transaction transaction
---
--- @return coroutine
+-- @param func(int)
 --
 function MySQL.Async.execute(query, params, func)
-    exports['mysql-async']:mysql_execute(query, safeParameters(params), func)
+    assert(type(query) == "string", "The SQL Query must be a string")
+
+    exports['mysql-async']:mysql_execute(query, safeParameters(params), safeCallback(func))
 end
 
 ---
@@ -52,34 +98,24 @@ end
 --
 -- @param query
 -- @param params
--- @param func
--- @param Transaction transaction
---
--- @return coroutine
+-- @param func(table)
 --
 function MySQL.Async.fetchAll(query, params, func)
-    exports['mysql-async']:mysql_fetch_all(query, safeParameters(params), func)
+    assert(type(query) == "string", "The SQL Query must be a string")
+
+    exports['mysql-async']:mysql_fetch_all(query, safeParameters(params), safeCallback(func))
 end
 
 ---
--- Execute a query and fetch the first column of the first row
+-- Execute a query and fetch the first column of the first row, async version
 -- Useful for count function by example
 --
 -- @param query
 -- @param params
--- @param func
--- @param Transaction transaction
---
--- @return coroutine
+-- @param func(mixed)
 --
 function MySQL.Async.fetchScalar(query, params, func)
-    exports['mysql-async']:mysql_fetch_scalar(query, safeParameters(params), func)
+    assert(type(query) == "string", "The SQL Query must be a string")
+
+    exports['mysql-async']:mysql_fetch_scalar(query, safeParameters(params), safeCallback(func))
 end
-
-AddEventHandler('onServerResourceStart', function (resource)
-    if resource == "mysql-async" then
-        MySQL:init()
-
-        TriggerEvent('onMySQLReady')
-    end
-end)
