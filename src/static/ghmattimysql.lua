d1347c2052250ee561c93611fedcef0e7abd0f52
Matthias Mandelartz <33543464+GHMatti@users.noreply.github.com>
2018-09-05 18:14:57 +0200

    format lua the same way as js

--- src/static/ghmattimysql.lua
+++ src/static/ghmattimysql.lua
@@ -1,32 +1,32 @@
 exports('executeSync', function (query, parameters)
-    local res = {}
-    local finishedQuery = false
-    exports.ghmattimysql:execute(query, parameters, function(result)
-        res = result
-        finishedQuery = true
-    end)
-    repeat Citizen.Wait(0) until finishedQuery == true
-    return res
+  local res = {}
+  local finishedQuery = false
+  exports.ghmattimysql:execute(query, parameters, function (result)
+    res = result
+    finishedQuery = true
+  end)
+  repeat Citizen.Wait(0) until finishedQuery == true
+  return res
 end)
 
 exports('scalarSync', function (query, parameters)
-    local res = {}
-    local finishedQuery = false
-    exports.ghmattimysql:scalar(query, parameters, function (result)
-        res = result
-        finishedQuery = true
-    end)
-    repeat Citizen.Wait(0) until finishedQuery == true
-    return res
+  local res = {}
+  local finishedQuery = false
+  exports.ghmattimysql:scalar(query, parameters, function (result)
+    res = result
+    finishedQuery = true
+  end)
+  repeat Citizen.Wait(0) until finishedQuery == true
+  return res
 end)
 
 exports('transactionSync', function (query, parameters)
-    local res = {}
-    local finishedTransaction = false
-    exports.ghmattimysql:transaction(query, parameters, function (result)
-        res = result
-        finishedTransaction = true
-    end)
-    repeat Citizen.Wait(0) until finishedTransaction == true
-    return res
+  local res = {}
+  local finishedTransaction = false
+  exports.ghmattimysql:transaction(query, parameters, function (result)
+    res = result
+    finishedTransaction = true
+  end)
+  repeat Citizen.Wait(0) until finishedTransaction == true
+  return res
 end)
