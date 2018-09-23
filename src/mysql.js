bb0495989140d9262aa01162d789141b11dad85b
Matthias Mandelartz <33543464+GHMatti@users.noreply.github.com>
2018-09-23 14:13:07 +0200

    Parse connection strings, ugly.

--- src/mysql.js
+++ src/mysql.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql');
 
-let connectionString = '';
+let config = {};
 let debug = 0;
 let pool;
 
@@ -83,11 +83,57 @@ global.exports('mysql_insert', (query, parameters, callback) => {
     });
 });
 
+function parseOptions(config, options) {
+    const cfg = config;
+    const opts = options.split('&');
+    opts.forEach((o) => {
+        const keyValue = o.split('=');
+        cfg[keyValue[0]] = keyValue[1];
+    });
+    return cfg;
+}
+
+function parseConnectingString(connectionString) {
+    if(/(?:database|initial\scatalog)=(?:(.*?);|(.*))/gi.test(connectionString)) {
+
+        let matches = (/(?:host|server|data\s?source|addr(?:ess)?)=(?:(.*?);|(.*))/gi.exec(connectionString));
+        const host = (matches) ? matches[1] || matches[2] : 'localhost';
+        matches = (/(?:Port)=(?:(.*?);|(.*))/gi.exec(connectionString));
+        const port = (matches) ? matches[1] || matches[2] : 3306;
+        matches = (/(?:user\s?(?:id|name)?|uid)=(?:(.*?);|(.*))/gi.exec(connectionString));
+        const user = (matches) ? matches[1] || matches[2] : 'root';
+        matches = (/(?:password|pwd)=(?:(.*?);|(.*))/gi.exec(connectionString));
+        const password = (matches) ? matches[1] || matches[2] : '';
+        matches = (/(?:database|initial\scatalog)=(?:(.*?);|(.*))/gi.exec(connectionString));
+        const database = (matches) ? matches[1] || matches[2] : '';
+        return { host, port, user, password, database, dateStrings: true };
+
+    } else if(/mysql:\/\//gi.test(connectionString)) {
+
+        let matches = /mysql:\/\/(.*?)(?::|@)(?:(.*)@)?(.*?)(?::(\d{1,5}))?\/(.*?)\?(.*)/gi.exec(connectionString);
+        const host = (matches[3]) ? matches[3] : 'localhost';
+        const port = (matches[4]) ? matches[4] : 3306;
+        const user = (matches[1]) ? matches[1] : 'root';
+        const password = (matches[2]) ? matches[2] : '';
+        const database = (matches[5]) ? matches[5] : '';
+        const config = { host, port, user, password, database };
+        const options = matches[6];
+        return parseOptions(config, options);
+
+    } else throw new Error('No valid connection string found');
+}
+
+let isReady = false;
 global.on('onServerResourceStart', (resourcename) => {
     if (resourcename == 'mysql-async') {
-        connectionString = global.GetConvar('mysql_connection_string', 'mysql://localhost/');
+        const connectionString = global.GetConvar('mysql_connection_string', 'mysql://localhost/');
+        config = parseConnectingString(connectionString);
         debug = global.GetConvarInt('mysql_debug', 0);
-        pool = mysql.createPool(connectionString);
+        pool = mysql.createPool(config);
         global.emit('onMySQLReady');
+        isReady = true;
+    }
+    if (isReady) {
+        // global.emit('onMySQLReady');
     }
 });
