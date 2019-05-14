8f5a4e7f659cda730eba88afb050f126417fe5a1
GHMatti <33543464+GHMatti@users.noreply.github.com>
2019-05-14 21:18:39 +0200

    Fixed the resource display thanks to Adri

--- mysql-async-client.js
+++ mysql-async-client.js
@@ -140,9 +140,20 @@ window.onNet('mysql-async:update-resource-data', (resourceData) => {
     }
   }
   if (arrayToSortAndMap.length > 0) {
-    arrayToSortAndMap.sort((a, b) => a.totalExecutionTime - b.totalExecutionTime);
+    arrayToSortAndMap.sort((a, b) => a.queryTime - b.queryTime);
     const len = arrayToSortAndMap.length;
     arrayToSortAndMap = arrayToSortAndMap.filter((_, index) => index > len - 31);
+    arrayToSortAndMap.sort((a, b) => {
+      const resourceA = a.resource.toLowerCase();
+      const resourceB = b.resource.toLowerCase();
+      let result = 0;
+      if (resourceA < resourceB) {
+        result = -1;
+      } else if (resourceA > resourceB) {
+        result = 1;
+      }
+      return result;
+    });
     NuiMessage({
       type: 'onResourceLabels',
       resourceLabels: arrayToSortAndMap.map(el => el.resource),
