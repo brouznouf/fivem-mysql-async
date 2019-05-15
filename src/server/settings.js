da0e125d40593ca10255e830964ab388ae39743f
GHMatti <33543464+GHMatti@users.noreply.github.com>
2019-05-15 10:21:44 +0200

    Improve connectionstr parser, break pwds with ';'

--- src/server/settings.js
+++ src/server/settings.js
@@ -1,33 +1,35 @@
 const { parseUrl } = require('mysql/lib/ConnectionConfig');
 
+const defaultCfg = {
+  host: 'localhost',
+  username: 'root',
+  database: 'fivem',
+  supportBigNumbers: true,
+  multipleStatements: true,
+};
+
 // add some more functionality and improve the C# connection string parsing
 function parseConnectingString(connectionString) {
-  let cfg = null;
+  let cfg = {};
   if (/(?:database|initial\scatalog)=(?:(.*?);|(.*))/gi.test(connectionString)) {
-    let matches = (/(?:host|server|data\s?source|addr(?:ess)?)=(?:(.*?);|(.*))/gi.exec(connectionString));
-    const host = (matches) ? matches[1] || matches[2] : 'localhost';
-    matches = (/(?:Port)=(?:(.*?);|(.*))/gi.exec(connectionString));
-    const port = (matches) ? matches[1] || matches[2] : 3306;
-    matches = (/(?:user\s?(?:id|name)?|uid)=(?:(.*?);|(.*))/gi.exec(connectionString));
-    const user = (matches) ? matches[1] || matches[2] : 'root';
-    matches = (/(?:password|pwd)=(?:(.*?);|(.*))/gi.exec(connectionString));
-    const password = (matches) ? matches[1] || matches[2] : '';
-    matches = (/(?:database|initial\scatalog)=(?:(.*?);|(.*))/gi.exec(connectionString));
-    const database = (matches) ? matches[1] || matches[2] : '';
-    cfg = {
-      host,
-      port,
-      user,
-      password,
-      database,
-      supportBigNumbers: true,
-      multipleStatements: true,
-    };
+    // replace the old version with the new one
+    const connectionStr = connectionString
+      .replace(/(?:host|server|data\s?source|addr(?:ess)?)=/gi, 'host=')
+      .replace(/(?:port)=/gi, 'port=')
+      .replace(/(?:user\s?(?:id|name)?|uid)=/gi, 'user=')
+      .replace(/(?:password|pwd)=/gi, 'password=')
+      .replace(/(?:database|initial\scatalog)=/gi, 'database=');
+    connectionStr.split(';').forEach((el) => {
+      const equal = el.indexOf('=');
+      const key = (equal > -1) ? el.substr(0, equal) : el;
+      const value = (equal > -1) ? el.substr(equal + 1) : '';
+      cfg[key] = value;
+    });
   } else if (/mysql:\/\//gi.test(connectionString)) {
     cfg = parseUrl(connectionString);
   } else throw new Error('No valid connection string found');
 
-  return cfg;
+  return Object.assign({}, defaultCfg, cfg);
 }
 
 module.exports = parseConnectingString;
