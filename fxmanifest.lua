f2c1d97cf624c5d311bf08d34f9a376c2f0f47dc
GHMatti <33543464+GHMatti@users.noreply.github.com>
2020-06-17 16:25:03 +0200

    chore: version

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ game 'common'
 name 'mysql-async'
 description 'MySQL Middleware for lua.'
 author 'Joel Wurtz & Matthias Mandelartz'
-version '3.2.3'
+version '3.3.0'
 url 'https://github.com/brouznouf/fivem-mysql-async'
 
 server_script 'mysql-async.js'
